acff3eb89af87a5eaafdef559d938c0c6decfdbf
ray <397593251@qq.com>
2021-04-08 17:45:47 +0800

    update

--- handlers/SioHandler.py
+++ handlers/SioHandler.py
@@ -4,58 +4,20 @@
 # @FILE: SioHandler.py
 # @AUTHOR: Ray
 import socketio
+
 sio = socketio.AsyncServer(async_mode='tornado')
 
-@sio.event
-async def my_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']}, room=sid)
+
+class RecordVideoNamespace(socketio.AsyncNamespace):
+    async def on_connect(self, sid, environ):
+        pass
+
+    async def on_disconnect(self, sid):
+        print(sid)
+        await sio.disconnect(sid)
+
+    async def on_my_event(self, sid, data):
+        await self.emit('my_response', data)
 
 
-@sio.event
-async def my_broadcast_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']})
-
-
-@sio.event
-async def join(sid, message):
-    sio.enter_room(sid, message['room'])
-    await sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
-                   room=sid)
-
-
-@sio.event
-async def leave(sid, message):
-    sio.leave_room(sid, message['room'])
-    await sio.emit('my_response', {'data': 'Left room: ' + message['room']},
-                   room=sid)
-
-
-@sio.event
-async def close_room(sid, message):
-    await sio.emit('my_response',
-                   {'data': 'Room ' + message['room'] + ' is closing.'},
-                   room=message['room'])
-    await sio.close_room(message['room'])
-
-
-@sio.event
-async def my_room_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']},
-                   room=message['room'])
-
-
-@sio.event
-async def disconnect_request(sid):
-    await sio.disconnect(sid)
-
-
-@sio.event
-async def connect(sid, environ):
-    print(environ['QUERY_STRING'])
-    # sio.start_background_task(background_task)
-    await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
-
-
-@sio.event
-def disconnect(sid):
-    print('Client disconnected')
+sio.register_namespace(RecordVideoNamespace('/record_video'))
